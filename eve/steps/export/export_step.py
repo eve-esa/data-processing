2836c9eac065318075a78f90775ff00631db37a4
JINO-ROHIT <find.jinorohit@gmail.com>
2025-09-10 09:54:16 +0530

    document object for export step

--- eve/steps/export/export_step.py
+++ eve/steps/export/export_step.py
@@ -1,20 +1,28 @@
-from pathlib import Path
 import aiofiles
+from pathlib import Path
+from typing import List, Union, Tuple
 
+from eve.model.document import Document
 from eve.base_step import PipelineStep
 
 class ExportStep(PipelineStep):
-    async def execute(self, input_data: list) -> list:
+    async def execute(self, input_data: Union[List[Document], List[Tuple[Path, str]]]) -> List[Document]:
         destination = Path(self.config.get("destination", "./output"))
 
         if not destination.exists():
             self.logger.info(f"{destination} does not exist. creating...")
             destination.mkdir(parents = True, exist_ok = True)
+        
+        documents = []
+        if input_data and isinstance(input_data[0], tuple):
+            documents = [Document.from_tuple(item) for item in input_data]
+        else:
+            documents = input_data
 
-        for file_path, content in input_data:
-            output_file = destination / f"{file_path.stem}.{self.config.get('format', 'md')}"
+        for document in documents:
+            output_file = destination / f"{document.filename}.{self.config.get('format', 'md')}"
             async with aiofiles.open(output_file, 'w', encoding='utf-8') as f:
-                await f.write(content)
+                await f.write(document.content)
             self.logger.info(f"Saved file: {output_file}")
 
         return input_data
