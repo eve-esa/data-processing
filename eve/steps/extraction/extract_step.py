94eb1298e163cae3e01c3aa9fd9781353cf2ea2a
elements72 <antonio.lopez.8523@gmail.com>
2025-10-07 17:50:38 +0200

    Added jsonl extractor

--- eve/steps/extraction/extract_step.py
+++ eve/steps/extraction/extract_step.py
@@ -2,73 +2,105 @@ from typing import List
 
 from eve.base_step import PipelineStep
 from eve.model.document import Document
+from eve.steps.cleaning.standalone import logger
 from eve.steps.extraction.htmls import HtmlExtractor
+from eve.steps.extraction.jsonl import JSONLExtractor
 from eve.steps.extraction.xmls import XmlExtractor
 from eve.steps.extraction.pdfs import PdfExtractor
 from eve.steps.extraction.markdown import MarkdownExtractor
+
 
 class ExtractionStep(PipelineStep):
     async def _html_extraction(self, document: Document) -> Document:
         html_extractor = HtmlExtractor(document)
         text = await html_extractor.extract_text()
         return text
-    
+
     async def _pdf_extraction(self, document: Document, url: str) -> Document:
         pdf_extractor = PdfExtractor(document, url)
         text = await pdf_extractor.extract_text()
         return text
-    
+
     async def _xml_extraction(self, document: Document) -> Document:
         xml_extractor = XmlExtractor(document)
         text = await xml_extractor.extract_text()
         return text
-    
+
     async def _md_extraction(self, document: Document) -> Document:
         md_extractor = MarkdownExtractor(document)
         text = await md_extractor.extract_text()
         return text
 
+    async def _jsonl_extraction(self, document: Document) -> List[Document]:
+        jsonl_extractor = JSONLExtractor(document)
+        documents = await jsonl_extractor.extract_documents()
+        return documents
+
     async def execute(self, documents: List[Document]) -> List[Document]:
         """Execute text extraction on input files or documents.
-        
+
         Args:
             input_data: List of file paths or Document objects to extract text from
-            
+
         Returns:
             List of Document objects with extracted text
         """
-        format = self.config.get("format", None)  # write a wrapper to find out the extension
+        format = self.config.get(
+            "format", None
+        )  # write a wrapper to find out the extension
         if not format:
             unique_formats = set()
 
         unique_formats = {document.file_format for document in documents}
-        
-        self.logger.info(f"Extracting text from {unique_formats} files. File count: {len(documents)}")
+        text_extraction_formats = ["html", "xml", "pdf", "md"]
+        supported_formats = ["jsonl"]
+
+        self.logger.info(
+            f"Extracting text from {unique_formats} files. File count: {len(documents)}"
+        )
 
         result = []
         for document in documents:
             try:
-                if document.file_format == "html":
-                    document_with_text = await self._html_extraction(document)
-                elif document.file_format == "pdf":
-                    url = self.config.get("url", None)
-                    if not url:
-                        self.logger.error("No URL provided for PDF extraction service")
-                    document_with_text = await self._pdf_extraction(document, url)
-                elif document.file_format == "xml":
-                    document_with_text = await self._xml_extraction(document)
-                elif document.file_format == 'md':
-                    document_with_text = await self._md_extraction(document)
-                else:
-                    self.logger.error(f"Unsupported format: {document.file_format}")
-                    continue
-                
-                if document_with_text and hasattr(document_with_text, 'content_length') and document_with_text.content_length > 1:
-                    result.append(document_with_text)
-                    self.logger.info(f"Successfully extracted {document_with_text.content_length} characters from {document_with_text.filename}")
+                if document in text_extraction_formats:
+                    if document.file_format == "html":
+                        document_with_text = await self._html_extraction(document)
+                    elif document.file_format == "pdf":
+                        url = self.config.get("url", None)
+                        if not url:
+                            self.logger.error(
+                                "No URL provided for PDF extraction service"
+                            )
+                        document_with_text = await self._pdf_extraction(document, url)
+                    elif document.file_format == "xml":
+                        document_with_text = await self._xml_extraction(document)
+                    elif document.file_format == "md":
+                        document_with_text = await self._md_extraction(document)
+                    else:
+                        self.logger.error(f"Unsupported format: {document.file_format}")
+                        continue
+
+                    if (
+                        document_with_text
+                        and hasattr(document_with_text, "content_length")
+                        and document_with_text.content_length > 1
+                    ):
+                        result.append(document_with_text)
+                        self.logger.info(
+                            f"Successfully extracted {document_with_text.content_length} characters from {document_with_text.filename}"
+                        )
+                elif document.file_format in supported_formats:
+                    docs = await self._jsonl_extraction(document)
+                    docs = docs or []
+                    result.extend(docs)
+                    self.logger.info(
+                        f"Successfully extracted {len(docs)} documents from {document.filename}"
+                    )
                 else:
                     self.logger.warning(f"No text extracted from {document.filename}")
             except Exception as e:
-                self.logger.error(f"Failed to extract text from {document.filename}: {str(e)}")
+                self.logger.error(
+                    f"Failed to extract text from {document.filename}: {str(e)}"
+                )
                 continue
         return result
