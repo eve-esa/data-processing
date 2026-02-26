a632e4e1c0ba00158f748da3fc2e436f8276f00c
JINO-ROHIT <find.jinorohit@gmail.com>
2026-02-26 13:49:11 +0530

    minor: pin paddlepaddle version

--- server/setup_monkeyocr.sh
+++ server/setup_monkeyocr.sh
@@ -17,7 +17,7 @@ echo "Using CUDA version: $CUDA_VERSION"
 
 pip install paddlepaddle-gpu==3.0.0 -i https://www.paddlepaddle.org.cn/packages/stable/cu${CUDA_VERSION}/
 pip install "paddlex[base]==3.1.4"
-pip install paddlepaddle
+pip install paddlepaddle==3.2.2
 pip install langchain==0.3.26
 pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu${CUDA_VERSION}
 pip install -e .
